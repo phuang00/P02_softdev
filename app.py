a6d1a42a347d3b1dff7014ab08930cf62a44eff6
phuang00 <phuang00@stuy.edu>
2020-01-07 09:17:13 -0500

    fixed styling

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def index():
     # load the template with the user's session info
     if 'user' in session:
         return redirect(url_for('home'))
-    else: return redirect(url_for('login'))
+    return redirect(url_for('login'))
 
 
 @app.route('/login')
@@ -24,15 +24,15 @@ def login():
     if 'user' in session:
         return redirect(url_for('home'))
     elif request.args:
-        if db_functions.checkfor_credentials(request.args.get('username'), request.args.get('password')):
+        if db_functions.checkfor_credentials(request.args.get('username'),
+                                             request.args.get('password')):
             session['user'] = request.args.get('username')
             session['password'] = request.args.get('password')
             session['id'] = db_functions.get_user_id(request.args.get('username'))
             return render_template('home.html')
-        else:
-            flash('Invalid Credentials')
-            return redirect(url_for('login'))
-    else: return render_template('login.html')
+        flash('Invalid Credentials')
+        return redirect(url_for('login'))
+    return render_template('login.html')
 
 @app.route('/register')
 def register():
@@ -42,32 +42,28 @@ def register():
         if db_functions.checkfor_username(request.args.get('username')):
             flash('Account with that username already exists')
             return redirect(url_for('register'))
-        else:
-            db_functions.create_user(request.args.get('username'), request.args.get('password'))
-            flash('Account Created')
-            return redirect(url_for('login'))
+        db_functions.create_user(request.args.get('username'), request.args.get('password'))
+        flash('Account Created')
+        return redirect(url_for('login'))
     return render_template('register.html')
 
 @app.route('/home')
 def home():
     if 'user' in session:
         return render_template('home.html')
-    else:
-        return redirect(url_for('login'))
+    return redirect(url_for('login'))
 
 @app.route('/create')
 def create():
     if 'user' in session:
         return render_template('create.html')
-    else:
-        return redirect(url_for('login'))
+    return redirect(url_for('login'))
 
 @app.route('/play')
 def play():
     if 'user' in session:
         return render_template('play.html')
-    else:
-        return redirect(url_for('login'))
+    return redirect(url_for('login'))
 
 if __name__ == "__main__":
     create_db.create()

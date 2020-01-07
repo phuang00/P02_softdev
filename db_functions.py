e7eeb06146b84f5de482dc0c266d16f79bf53de8
Jacob Olin <jolin00@stuy.edu>
2020-01-07 09:25:40 -0500

    Added IF NOT EXISTS to creation of tables

---
+++ db_functions.py
@@ -0,0 +1,52 @@
+import sqlite3  # enable control of an sqlite database
+
+# checkfor_credentials()
+# - @return username and password of accounts that meet the credentials in the password (either an empty touple or 1-sized touple)
+def checkfor_credentials(username, password):
+    DB_FILE = "jeopardy.db"
+    db = sqlite3.connect(DB_FILE)  # open if file exists, otherwise create
+    c = db.cursor()  # facilitate db ops
+
+    query = "SELECT username, password FROM users WHERE users.username = \"%s\" AND users.password = \"%s\";" % (username, password)
+    response = list(c.execute(query))
+    db.commit()  # save changes
+    db.close()  # close database
+
+    return response
+
+
+def checkfor_username(username):
+    DB_FILE = "jeopardy.db"
+    db = sqlite3.connect(DB_FILE)  # open if file exists, otherwise create
+    c = db.cursor()  # facilitate db ops
+
+    query = "SELECT username FROM users WHERE username == \"%s\";" % (username)
+    response = list(c.execute(query))
+    db.commit()  # save changes
+    db.close()  # close database
+
+    return response
+
+
+def create_user(username, password):
+    DB_FILE = "jeopardy.db"
+    db = sqlite3.connect(DB_FILE)  # open if file exists, otherwise create
+    c = db.cursor()  # facilitate db ops
+
+    query = "INSERT INTO users(username, password) VALUES(\"%s\", \"%s\");" % (username, password)
+    response = list(c.execute(query))
+    db.commit()  # save changes
+    db.close()  # close database
+
+    return response
+
+def get_user_id(username):
+    DB_FILE = "jeopardy.db"
+    db = sqlite3.connect(DB_FILE)  # open if file exists, otherwise create
+    c = db.cursor()  # facilitate db ops
+
+    query = "SELECT user_id FROM users WHERE username == \"%s\";" % (username)
+    response = list(c.execute(query))
+    db.commit()  # save changes
+    db.close()  # close database
+    return response

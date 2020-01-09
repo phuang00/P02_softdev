217417ee2d1ec8465daa7ebf88a2532374099cbc
Alice <ani00@stuy.edu>
2020-01-08 21:34:38 -0500

    added code for APIs

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ from flask import url_for
 from utl import db_functions, create_db
 import sqlite3  # enable control of an sqlite database
 import os
+import json
+import urllib
+import random
 
 app = Flask(__name__)  # create instance of class Flask
 app.secret_key = os.urandom(24)
@@ -73,7 +76,56 @@ def customize():
 
 @app.route('/play')
 def play():
-    if request.args:
+    if 'user' in session:
+
+        # COUNTRIES API
+        pop = urllib.request.urlopen("https://restcountries.eu/rest/v2/")
+        data = [json.loads(pop.read())]
+        x = 25
+        h = 25
+        d = ""
+        # Random countries and flags
+        while x > 0:
+            y = random.randint(-1,249)
+            d = d + data[0][y]['name'] + " "
+            # line to append to database, gets random country
+            pics = data[0][y]['flag']
+            # line to append to database, gets corresponding flag
+            x = x - 1
+
+        # RICK AND MORTY API
+        rick = urllib.request.urlopen("https://rickandmortyapi.com/api/character")
+        morty = [json.loads(rick.read())]
+        n = 25
+        # Random character images and names
+        while n > 0:
+            p = random.randint(-1,19)
+            pic = morty[0]['results'][p]['image']
+            # line to append to database, gets random image
+            name = morty[0]['results'][0]['name']
+            # line to append to database, gets random name
+            n = n - 1
+
+        # POKE API (doesn't work idk whyyyyy)
+        #poke = urllib.request.urlopen("https://pokeapi.co/api/v2/pokemon/")
+        #mon = [json.loads(poke.read())]
+        #m = 25
+        #q = mon[0]['results'][0]['url']
+        #w = urllib.request.urlopen(q)
+        #monpics = [json.loads(v.read())]
+        #picz = monpics[0]['sprites']['front_default']
+
+        # OPEN TRIVIA API  ( requires a session token, they expire after six hours )
+        trivia = urllib.request.urlopen("https://opentdb.com/api.php?amount=25")
+        questions = [json.loads(trivia.read())]
+        m = 0;
+        while m <= 25:
+            question = questions[m]['results'][m]['question']
+            # line append question to database
+            answer = questions[m]['results'][m]['correct_answer']
+            # line to append answer to database
+            m += 1
+            
         return render_template('play.html', board_name = request.args.get('board_name'))
     return redirect(url_for('login'))
 

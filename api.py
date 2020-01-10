1357c4fc406eed08e391507b468402179c5e91d0
Alice <ani00@stuy.edu>
2020-01-09 20:54:25 -0500

    added poke api

--- api.py
+++ api.py
@@ -3,6 +3,7 @@ import json
 import urllib
 import random
 import urllib.request
+from urllib.request import Request, urlopen
 
 # COUNTRIES API
 def getCountries():
@@ -57,9 +58,34 @@ def getRickAndMorty():
         # answer
         d.append(data[0]['results'][n]['name'])
         l = l + 1
+    return
 
-    print(d)
+headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+    'Accept-Encoding': 'none',
+    'Accept-Language': 'en-US,en;q=0.8',
+    'Connection': 'keep-alive'
+}
+
+# POKE API
+def getPokemon():
+    request = Request("https://pokeapi.co/api/v2/pokemon/?offset=100&limit=100", headers=headers)
+    response = urlopen(request).read()
+    data = json.loads(response)
+    m = 25
+    d = []
+    while m >= 0:
+        # question
+        w = Request(data['results'][m]['url'], headers=headers)
+        mon = urlopen(w).read()
+        monpics = [json.loads(mon)]
+        d.append(monpics[0]['sprites']['front_default'])
+        # answer
+        d.append(data['results'][m]['name'])
+        m = m - 1
 
     return
 
-getRickAndMorty()
+

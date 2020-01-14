66037b1cca7229836075cc5dd364b32ff4553c30
phuang00 <phuang00@stuy.edu>
2020-01-14 15:08:51 -0500

    got /play to allow users to set the number of teams and the team names

--- static/base.js
+++ static/base.js
@@ -11,4 +11,31 @@ var getAns = function(){
     x.style.fontSize = "30px";
 }
 
+var addTeams = function(){
+            var number = document.getElementById("teams").value;
+            console.log(number);
+            var container = document.getElementById("numteams");
+            while (container.hasChildNodes()) {
+                container.removeChild(container.lastChild);
+            }
+            var row = container.appendChild(document.createElement("div"));
+            for (i = 0; i < number; i++){
+                var label = container.appendChild(document.createElement("label"));
+                label.innerHTML = "Team " + (i + 1);
+                label.for = "team" + (i + 1);
+                label.classList.add("col-auto");
+                label.classList.add("col-form-label");
+                var input = document.createElement("input");
+                input.type = "text";
+                input.name = "team" + (i + 1);
+                container.appendChild(input);
+                container.appendChild(document.createElement("br"));
+                container.appendChild(document.createElement("br"));
+            }
+            var btn = container.appendChild(document.createElement("input"));
+            btn.classList.add("btn");
+            btn.classList.add("btn-outline-success");
+            btn.type = "submit";
+        }
+
 revealAns()
